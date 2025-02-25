c24792b64855c7bd23984fac3c0c71ca8b9d9451
sarthoksarik <kirasnamdas@gmail.com>
2025-02-25 15:15:21 +0600

    filefolderid

--- track_sms.py
+++ track_sms.py
@@ -1,9 +1,9 @@
 from sms_track import SMSTRACK
 if __name__ == '__main__':
     # Configuration
-    CREDENTIALS_PATH = 'credentials.json'
-    SOURCE_FOLDER_ID = 'your_source_folder_id'
-    TARGET_SHEET_ID = 'your_target_sheet_id'
+    CREDENTIALS_PATH = 'creds.json'
+    SOURCE_FOLDER_ID = '12P0yrMcumb-2-8t0FNxlGXzUomoya8re'
+    TARGET_SHEET_ID = '1ATCR6d6OfAqPGdozoyA_iLOybRkj7h2-VL7do5ZVuOM'
 
     # Create manager instance
     sheet_manager = SMSTRACK(CREDENTIALS_PATH)
