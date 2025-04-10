02c4d48135cf19ecfe3f1c1ca2de42ce7c178698
Sarik Sadman <kirasnamdas@gmail.com>
2025-04-10 17:40:30 +0600

    systemd

--- systemd/deploy.sh
+++ systemd/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Copy systemd files to appropriate location
-sudo cp systemd/smstrack.* /etc/systemd/system/
+#sudo cp systemd/smstrack.* /etc/systemd/system/
+sudo cp systemd/call_log_track.* /etc/systemd/system/
 
 # Reload systemd daemon
 sudo systemctl daemon-reload
 
 # Enable and start timer
-sudo systemctl enable smstrack.timer
-sudo systemctl start smstrack.timer
+#sudo systemctl enable smstrack.timer
+#sudo systemctl start smstrack.timer
+
+sudo systemctl enable call_log_track.timer
+sudo systemctl start call_log_track.timer
 
 # Check status
-systemctl status smstrack.timer
+# systemctl status smstrack.timer
